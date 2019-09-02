3b4aa0db8559f8a7a7e3853630efa67def4f30ef
Mic Crook <crook@localhost.localdomain>
2019-09-02 16:21:40 -0400

    feature(API):user can sign in [Finishes#168247088]

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 const { JWT_SECRET } = process.env;
 
 class UsersController {
+  // User can sign up
   static userSignUp(req, res) {
     const {
       firstname, lastname, email, password, address, occupation, expertise,
@@ -33,6 +34,27 @@ class UsersController {
       data: {
         token: user,
       },
+    });
+  }
+
+  // User can sign in
+  static userSignIn(req, res) {
+    const { email } = req.body;
+    const { password } = req.body;
+    const user = Users.find((u) => u.email === email && u.password === password);
+    if (user) {
+      const userToken = jwt.sign({ email }, JWT_SECRET, {});
+      user.token = userToken;
+
+      return res.status(200).json({
+        status: 200,
+        message: 'User is successfully logged in',
+        data: user,
+      });
+    }
+    return res.status(404).json({
+      status: 404,
+      message: 'User not Found',
     });
   }
 }
--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -6,5 +6,7 @@ const usersRoute = express.Router();
 
 // User can sign up
 usersRoute.post('/auth/signup', UsersController.userSignUp);
+// User can signin
+usersRoute.post('/auth/signin', UsersController.userSignIn);
 
 export default usersRoute;
